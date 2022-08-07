9d7ca9c19da5d5d67286d302ec2d3e9852bccb07
Bathiya Ladduwahetty <dbladduwahetty@gmail.com>
2022-08-07 09:47:11 +0530

    Add logs 8

---
+++ src/main/java/com/modulec/EmployeeFunctions2.java
@@ -0,0 +1,67 @@
+package com.modulec;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class EmployeeFunctions2 {
+	
+	Logger logger = LogManager.getLogger(EmployeeFunctions2.class);
+	
+	String fileName;
+	EmployeeFunctions2(String fileName){
+		this.fileName = fileName;
+	}
+	
+	public void doSomething2() throws IOException {
+		  OutputStream stream = null;
+		  String[] propertyList = {"code", "code5"};
+		  try {
+		    for (String property : propertyList) {
+		      stream = new FileOutputStream("myfile.txt");
+		      logger.info(property);
+		    }
+		  } catch (Exception e) {
+		    // ...
+		  } finally {
+		    stream.close();
+		  }
+	}
+	
+	private void readTheEmployeeLog(){
+		Path path = Paths.get(this.fileName);
+		BufferedReader reader;
+		try {
+			reader = Files.newBufferedReader(path);
+			logger.info(reader.read());
+		} catch (IOException e1) {
+			logger.error("IOException");
+		}
+		
+		
+		try {
+			Stream<String> input = Files.lines(path);
+			input.forEach(logger::info);
+		}catch (FileNotFoundException e) {
+			logger.error("Invalid file name, check for the correct file ");
+		} catch (IOException e) {
+			logger.error("IOException");
+		}
+	}
+	
+	public void processEmployeeLogData(){
+		readTheEmployeeLog();
+	}
+	
+
+}
